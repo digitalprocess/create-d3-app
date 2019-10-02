523b76e44ea0b2df5d91217400d5031e875aba7c
frontendista <csdelreal@gmail.com>
2019-10-02 18:05:44 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import ChartWrapper from './ChartWrapper';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Navbar bg="light" className="justify-content-center">
+				<Navbar.Brand>CreateD3App</Navbar.Brand>
+			</Navbar>
+			<Container>
+				<Row>
+					<Col md={6} xs={12}><ChartWrapper /></Col>
+					<Col md={6} xs={12}></Col>
+				</Row>
+			</Container>
+		</div>
+	);
 }
 
 export default App;
